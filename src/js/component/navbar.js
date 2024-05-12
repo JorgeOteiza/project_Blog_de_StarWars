a34902833a6b6ec2849935c3b15dc44802b4062a
Jorge Cancino Oteíza <139603426+JorgeOteiza@users.noreply.github.com>
2024-05-12 09:22:45 +0000

    detalle prueba bg

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar-light bg-black mb-5">
+    <nav className="navbar-light bg-black mb-3">
       <div className="navbar mx-5 px-5">
       <Link to="/">
         <span className="navbar-brand my-3 h1">
