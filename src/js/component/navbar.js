b97ca1a4345fdc25a06a2cc68d2ce1501319f8c1
Jorge Cancino Oteíza <139603426+JorgeOteiza@users.noreply.github.com>
2024-05-12 08:42:49 +0000

    editando estilos, se agregarn las cards

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light mb-3">
+    <nav className="navbar-light bg-black mb-5">
+      <div className="navbar mx-5">
       <Link to="/">
-        <span className="navbar-brand mb-0 h1">
+        <span className="navbar-brand my-3 h1">
           <img
-            src="https://w7.pngwing.com/pngs/441/131/png-transparent-yoda-star-wars-logo-star-wars-text-star-wars-episode-vii-desktop-wallpaper.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png"
             alt="Star Wars"
             className="img-fluid"
             style={{ maxWidth: "100px", maxHeight: "100px" }}
           />
         </span>
       </Link>
-      <div className="ml-auto">
+      <div className="ml-auto my-3">
         <Link to="/demo">
           <button className="btn btn-primary">
-            y aquí el botón lista de favoritos
+            Favoritos
           </button>
         </Link>
+      </div>
       </div>
     </nav>
   );
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,57 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-  <div className="text-center mt-5">
-    <h1>Blog de StarWars</h1>
-    <p>
-      <img src={rigoImage} />
-    </p>
-    <a href="#" className="btn btn-success">
-      Aquí va la target de bootstrap
-    </a>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <h1 className="text-center mt-5 mb-4">Blog de StarWars</h1>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-md-12 d-flex">
+        <div className="card col-md-4 mb-3 mx-3">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card col-md-4 mb-3 mx-3">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card col-md-4 mb-3 mx-3">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
