7ddeb705349856bfafe7b6a517087872a7c0295e
Jorge Cancino Oteíza <139603426+JorgeOteiza@users.noreply.github.com>
2024-05-12 18:02:18 +0000

    instalación e importación de fontawesome, pintando estilos

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "../../styles/home.css";
 
 export const Home = () => (
   <div className="container">
     <div className="row">
       <div className="col-md-12">
-        <h1 className="text-center my-5">Blog de StarWars</h1>
+        <h1 className="text-center my-5">Star Wars Blog</h1>
       </div>
     </div>
-    <div className="card-container row overflow-auto">
-      <div className="col-md-12 d-flex my-5 py-5 w-auto">
+    <div className="card-container">
+      <div className="col-md-12 container d-flex justify-content-around my-5 py-3 row-cols-lg-4 w-auto">
         <div className="card col-md-4 mb-3 mx-3">
           <img className="card-img-top" src="https://starwars-visualguide.com/assets/img/categories/character.jpg" alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Entity</h5>
             <p className="card-text">
-identificar a c/u con su fetch/id
+              identificar a c/u con su fetch/id
             </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
+            <div className="container learnmore-fav">
+
+              <button type="button" class="btn btn-outline-primary">Learn more!</button>
+              <button class="btn btn-primary" type="submit"><FontAwesomeIcon icon={faHeart} /></button>
+
+            </div>
           </div>
         </div>
         <div className="card col-md-4 mb-3 mx-3">
@@ -27,11 +32,14 @@ identificar a c/u con su fetch/id
           <div className="card-body">
             <h5 className="card-title">vehicles</h5>
             <p className="card-text">
-dirigir contenido a single y crear buttons "leer más" y "agregar a favoritos"
+              dirigir contenido a single y crear buttons "leer más" y "agregar a favoritos"
             </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
+            <div className="container learnmore-fav">
+
+              <button type="button" class="btn btn-outline-primary">Learn more!</button>
+              <button class="btn btn-primary" type="submit"><FontAwesomeIcon icon={faHeart} /></button>
+
+            </div>
           </div>
         </div>
         <div className="card col-md-4 mb-3 mx-3">
@@ -39,13 +47,14 @@ dirigir contenido a single y crear buttons "leer más" y "agregar a favoritos"
           <div className="card-body">
             <h5 className="card-title">Planets</h5>
             <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
+              crear nuevo archivo y ruta en layout para favoritos.
             </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
+            <div className="container learnmore-fav">
+
+              <button type="button" class="btn btn-outline-primary">Learn more!</button>
+              <button class="btn btn-primary" type="submit"><FontAwesomeIcon icon={faHeart} /></button>
+
+            </div>
           </div>
         </div>
       </div>
