95326e23a41db3fa9ddfdeaf428077fc0c3390cd
Jorge Oteiza <oteiza.jor@gmail.com>
2024-07-23 17:47:08 +0000

    Reorganización estructura, orden de carpetas de interfaz StarWars y archivos de API

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./project_Blog_de_StarWars/src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
+        },
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "4geeks.ico",
+      template: "template.html",
+    }),
+    new Dotenv({ safe: true, systemvars: true }),
+  ],
+};
